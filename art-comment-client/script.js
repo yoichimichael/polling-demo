ccedbf7a27eddfbb2b0791867ce323d46bd8c9d9
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-10-07 00:59:58 -0400

    tested setTimeout function

--- art-comment-client/script.js
+++ art-comment-client/script.js
@@ -1,4 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("page loaded")
-    
-});
+document.addEventListener("DOMContentLoaded", addParagraphElement());
+
+function addParagraphElement() {
+	let commentsDiv = document.querySelector('div.comments')
+	setTimeout(() => {
+		commentsDiv.innerHTML = "<p>Like staring into the face of god</p>"
+	}, 5000); 
+}
