45f3b80067cca2c0c225def9c45365897efaa0aa
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-10-08 14:16:10 -0400

    added fetch function and display logic for comments for client

--- art-comment-api/app/controllers/comments_controller.rb
+++ art-comment-api/app/controllers/comments_controller.rb
@@ -5,7 +5,8 @@ class CommentsController < ApplicationController
 		render json: comments
 	end
 
-	def comment_params
-		params.require
-	end
+	# def comment_params
+	# 	params.require
+	# end
+	
 end
