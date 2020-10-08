454b65ebeb4948b948b868672e1f2e4c7e5d2e12
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-10-08 00:59:11 -0400

    created database and added seed data to api

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 ---
 This is a small program designed to show polling
+
+### To Do:
+#### API
+1. add seeds to API
+2. add routes
+3. flesh out model
+4. add controller
+
+#### client
+1. add data from API to DOM at DOMContentLoaded event
--- art-comment-api/db/seeds.rb
+++ art-comment-api/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+comments = [{ content: "Like staring into the face of God", name: "Mark Hamill" }, { content: "And they said painting was dead", name: "George Castanza" }, { content: "This is why I wake up in the morning", name: "Pharrel Williams"}]
+
+comments.each{|comment| Comment.create(content: comment[:content], name: comment[:name])}
