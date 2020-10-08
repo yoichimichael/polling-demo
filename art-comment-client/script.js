dfd5998bd0961d2751ad92a5d4a455c97e93f9b2
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-10-08 14:16:10 -0400

    added fetch function and display logic for comments for client

--- art-comment-client/script.js
+++ art-comment-client/script.js
@@ -1,8 +1,16 @@
+const COMMENTS_URL = 'http://localhost:3000/comments'
+let commentsDiv = document.querySelector('div.comments')
+
 document.addEventListener("DOMContentLoaded", addParagraphElement());
 
-function addParagraphElement() {
-	let commentsDiv = document.querySelector('div.comments')
-	setTimeout(() => {
-		commentsDiv.innerHTML = "<p>Like staring into the face of god</p>"
-	}, 5000); 
+function addParagraphElement() {	
+	fetch(COMMENTS_URL)
+	.then(resp => resp.json())
+	.then(comments => {
+		comments.forEach(comment => {
+			let p = document.createElement('p')
+			p.innerText = `${comment.content} - ${comment.name}`
+			commentsDiv.appendChild(p)
+		});
+	})
 }
