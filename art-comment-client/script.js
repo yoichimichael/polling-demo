32a031e5dc8688db9a21739302ca1278342fc660
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-10-11 22:55:11 -0400

    implementing long polling function

--- art-comment-client/script.js
+++ art-comment-client/script.js
@@ -9,8 +9,9 @@ const HEADERS = {
 const commentForm = document.querySelector('.comment-form');
 const commentsDiv = document.querySelector('div.comments');
 
-//event listeners
-document.addEventListener("DOMContentLoaded", fetchComments());
+// event listeners
+// swap second argument of DOMContentLoaded event to switch polling techniques
+document.addEventListener("DOMContentLoaded", asyncFetchComments());
 commentForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(e) {
@@ -22,8 +23,40 @@ function handleSubmit(e) {
 }
 
 
+// regular polling technique
+// sends a get request  to API for comments every 3 seconds
 function fetchComments() {
 	let interval = setInterval(getFetch, 3000, COMMENTS_URL);
+}
+
+async function asyncFetchComments() {
+	let response = await fetch(COMMENTS_URL);
+	return response.json();
+}
+
+// long polling technique
+async function asyncFetchComments() {
+	let response = await fetch(COMMENTS_URL);
+
+	if (response.status == 502) {
+		// Status 502 is a connection timeout error,
+		// may happen when the connection was pending for too long,
+		// and the remote server or a proxy closed it
+		// let's reconnect
+		await asyncFetchComments();
+	} else if (response.status != 200) {
+		// An error - let's show it
+		showMessage(response.statusText);
+		// Reconnect in one second
+		await new Promise(resolve => setTimeout(resolve, 1000));
+		await asyncFetchComments();
+	} else {
+		// Get and show the message
+		let message = await response.text();
+		showMessage(message);
+		// Call subscribe() again to get the next message
+		await asyncFetchComments();
+	}
 }
 
 
